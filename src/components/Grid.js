ea299c1af2f9613379b6104b4c213a6daa333420
tsaristbomba <andrexerdna@gmail.com>
2020-07-04 11:34:03 -0300

    scoreboard + bug correction

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -11,6 +11,9 @@ const Grid = () => {
   const [match, setMatch] = useState(null);
   const [count, setCount] = useState(null);
   const [start, setStart] = useState(false);
+  const [time, setTime] = useState(60);
+  const [name, setName] = useState(null);
+  const [topScore, setTop] = useState([]);
 
   useEffect(() => {
     getCards();
@@ -19,7 +22,17 @@ const Grid = () => {
   useEffect(() => {
     if (count === 0) {
       setStart(false);
-      return window.alert(`Time is up! You've got ${match} points!`);
+
+      if (localStorage.name) {
+        Number(localStorage.name) > match &&
+          localStorage.setItem(name.toUpperCase(), match);
+      } else {
+        localStorage.setItem(name.toUpperCase(), match);
+      }
+
+      window.alert(`Time is up! You've got ${match} points!`);
+
+      return window.location.reload();
     }
 
     if (count > 0) {
@@ -35,18 +48,24 @@ const Grid = () => {
     count === 0 && setStart(false);
   }, [count]);
 
+  useEffect(() => {
+    const arr = [];
+    for (var key in localStorage) {
+      if (localStorage.hasOwnProperty(key)) {
+        arr.push({ name: key, score: localStorage[key] });
+      }
+    }
+    setTop(arr);
+  }, []);
+
   const getCards = async () => {
     setLoading(true);
     try {
       const n = Math.floor(Math.random() * 400 + 1);
 
-      console.log(n);
-
       const res = await axios.get(
         `http://api.jikan.moe/v3/anime/${n}/characters_staff`
       );
-
-      console.log(res.data.characters.length);
 
       const arr = [];
 
@@ -68,8 +87,6 @@ const Grid = () => {
       setCards(arr);
       setLoading(false);
     } catch (err) {
-      console.log(err);
-
       const n = 1;
 
       const res = await axios.get(
@@ -118,48 +135,122 @@ const Grid = () => {
           document.getElementById(key).style.display = "none";
           document.getElementById(key + 10).style.display = "block";
           setStart(true);
-        }, 1000);
+        }, 500);
         setCurrent(null);
       } else {
         setStart(false);
 
         setCurrent(null);
-        setMatch(match + 1);
+        time === 60 && setMatch(match + 1);
+        time === 45 && setMatch(match + 2);
+        time === 30 && setMatch(match + 4);
         setTimeout(() => {
           setStart(true);
-        }, 1000);
+        }, 500);
       }
     }
   };
 
   const handleStart = () => {
-    setStart(true);
-    setCount(60);
-    setMatch(0);
-    getCards();
+    const name = prompt("Who's playing?");
+    if (name !== null) {
+      setName(name);
+
+      setStart(true);
+      setCount(time);
+      setMatch(0);
+      getCards();
+    }
+  };
+
+  const handleChange = (e) => {
+    const diff = e.target.value;
+
+    diff === "easy" && setTime(60);
+    diff === "medium" && setTime(45);
+    diff === "hard" && setTime(30);
   };
 
   return (
     <div className="grid">
-      <h1>記憶</h1>
+      {count === 0 || count === null ? (
+        <button
+          className="btn"
+          type="button"
+          onClick={() => window.location.reload()}
+        >
+          <h1 className="logo">記憶</h1>
+        </button>
+      ) : (
+        <button
+          className="btn"
+          type="button"
+          onClick={() => window.location.reload()}
+        >
+          <h1 className="primary-logo">記憶</h1>
+        </button>
+      )}
       <div className="btns">
-        <span>
-          <button className="btn " type="button" onClick={() => getCards()}>
-            <span>
-              Next Cards <i className="fas fa-step-forward"></i>
-            </span>
-          </button>
+        <form>
+          <input
+            id="easy"
+            type="radio"
+            name="difficulty"
+            value="easy"
+            defaultChecked
+            disabled={count > 0}
+            onChange={(e) => handleChange(e)}
+          />
+          <label htmlFor="easy"> Easy </label>
+          <input
+            id="medium"
+            type="radio"
+            name="difficulty"
+            value="medium"
+            disabled={count > 0}
+            onChange={(e) => handleChange(e)}
+          />
+          <label htmlFor="medium"> Medium </label>
+          <input
+            id="hard"
+            type="radio"
+            name="difficulty"
+            value="hard"
+            disabled={count > 0}
+            onChange={(e) => handleChange(e)}
+          />
+          <label htmlFor="hard"> Hard </label>
+        </form>
 
-          <button className="btn " type="button" onClick={() => handleStart()}>
-            {count === 0 || count === null ? (
+        <span>
+          {count !== null && (
+            <button className="btn " type="button" onClick={() => getCards()}>
+              <span>
+                Next Cards <i className="fas fa-step-forward"></i>
+              </span>
+            </button>
+          )}
+
+          {count === 0 || count === null ? (
+            <button
+              className="btn "
+              type="button"
+              onClick={() => handleStart()}
+            >
               <i className="larger danger fas fa-power-off"></i>
-            ) : (
+            </button>
+          ) : (
+            <button
+              className="btn "
+              type="button"
+              onClick={() => window.location.reload()}
+            >
               <i className="larger primary fas fa-power-off"></i>
-            )}
-          </button>
+            </button>
+          )}
         </span>
         {count !== null && (
-          <p>
+          <p className="scoreboard">
             Points: {match} | Timer: {count}
           </p>
         )}
@@ -181,6 +272,25 @@ const Grid = () => {
             <div id={k + 10} className="back" style={{ display: `block` }} />
           </div>
         ))
+      )}
+      {localStorage.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Score</th>
+            </tr>
+          </thead>
+          <tbody>
+            {topScore.length > 0 &&
+              topScore.map((data, key) => (
+                <tr key={key}>
+                  <td>{data.name}</td>
+                  <td>{data.score}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
       )}
     </div>
   );
